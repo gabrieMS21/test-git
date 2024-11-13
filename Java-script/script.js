d76ad8e640b504df3618112580fc0417883e2dbf
SamurayDev <gabriel.m.s2100@gmail.com>
2024-11-13 17:07:51 -0300

    tests

---
+++ Java-script/script.js
@@ -0,0 +1,6 @@
+
+var nome, sobreNome, nomeCompleto;
+nome = "Gabriel";
+sobreNome = "Mendes Santos";
+nomeCompleto = nome + " " + sobreNome;
+document.getElementById("texto").innerHTML = nomeCompleto;
